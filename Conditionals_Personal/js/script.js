ac118150704d3bc3c850c8f2e280096dead56422
bria.alyse <bria.alyse@fullsail.edu>
2014-02-20 21:12:04 -0500

    Added logical and rational operations. If you are 16 and passed the permit test you can take driving test.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -3,12 +3,13 @@
 var teenAge = 19;
 var minAge = 18;
 var wPermitAge = 16
+var permitTest = passed
 
 // if the teen is 18 or older, print to the console "you may take the driving test!"
 if(teenAge > minAge){
 	//code performed if condition is true.
 	console.log("You may take the driving test!");
-}else if (teenAge > wPermitAge) {
+}else if (teenAge > wPermitAge && permitTest = passed) {
 	//you can take the test if you have your permit
 	console.log ("if you have your permit, you can take the driving test.")
 	
