dabdecc8a1eb3f7b95d3b1b663fe8a02c311696d
bria.alyse <bria.alyse@fullsail.edu>
2014-02-20 20:30:19 -0500

    Commiting variable and Conditional. The Conditional is that you have to be of age to take the driving test.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,5 +1,9 @@
 // Personal Conditional
 
-var ofAge = true:
+var ofAge = true;
 
 // if the teen is of age, print to the console "you may take the driving test!"
+if(ofAge){
+	console.log("You may take the driving test!")
+
+}
