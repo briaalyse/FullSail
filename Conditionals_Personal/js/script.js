ce5c8bea2b0d63042a072ff5bb0558adddfd03cb
bria.alyse <bria.alyse@fullsail.edu>
2014-02-20 20:41:36 -0500

    Modified Conditional. The minimum age is 18, but the teen age is 19.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,11 +1,11 @@
 // Personal Conditional
 
-var teenAge = 18;
+var teenAge = 19;
+var minAge = 18;
 
 // if the teen is 18 or older, print to the console "you may take the driving test!"
 if(teenAge > 18){
 	//code performed if condition is true.
 	console.log("You may take the driving test!");
-
-
 }
+else
