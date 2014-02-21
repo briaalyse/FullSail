82091df149f29eed89e6501ee380c74db99c9312
bria.alyse <bria.alyse@fullsail.edu>
2014-02-20 21:03:14 -0500

    committing else if condition. If the teen is less than 18, they have to have a permit in order to take the driving test.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -2,12 +2,17 @@
 
 var teenAge = 19;
 var minAge = 18;
+var wPermitAge = 16
 
 // if the teen is 18 or older, print to the console "you may take the driving test!"
 if(teenAge > minAge){
 	//code performed if condition is true.
 	console.log("You may take the driving test!");
-}else{
-	//code performed if condition is false.
+}else if (teenAge > wPermitAge) {
+	//you can take the test if you have your permit
+	console.log ("if you have your permit, you can take the driving test.")
+	
+} else{
+	//you are not of age to obtain your license
 	console.log ("You are not of age to obtain your license.")
 }
