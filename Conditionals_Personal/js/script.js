9126787afc177ff52f3e9930cc90f8368eb61ddb
bria.alyse <bria.alyse@fullsail.edu>
2014-02-20 20:45:32 -0500

    Added a conditional. If teen is not older than 18, they cannot obtain their license.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -4,8 +4,11 @@ var teenAge = 19;
 var minAge = 18;
 
 // if the teen is 18 or older, print to the console "you may take the driving test!"
-if(teenAge > 18){
+if(teenAge > minAge){
 	//code performed if condition is true.
 	console.log("You may take the driving test!");
 }
-else
+if (teenAge < minAge){
+	//code performed if the teen age is less than 18, print to console "You are not of age to obtain your license."
+	console.log ("You are not of age to obtain your license.")
+}
