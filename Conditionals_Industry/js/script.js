1b05ce9df8411a3a05f3d7c04e5236b2a81ce8bb
bria.alyse <bria.alyse@fullsail.edu>
2014-02-21 23:41:45 -0500

    Edited a mistake in first conditional.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -5,9 +5,10 @@ var minKnowledge = 50
 var guideToAdobe = degree
 
 // in order to be a good web designer you need at least 50 in knowledge to understand what you are doing
-if(adobeKnowledge < 50){ 
-	console.log ("You will need more knowledge to be a good web designer")
+if(adobeKnowledge > 50){ 
+	console.log ("You will be a good web designer")
 }else if{
 	(adobeKnowledge < minKnowledge && guideToAdobe = degree) console.log("you will do okay, but you will need much practice")
 
 }
+(adobeKnowledge > minKnowledge) ? console.log("")
