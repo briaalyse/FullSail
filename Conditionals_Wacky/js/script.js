2c36d533058eb5ce4e897effcf2b8257f500eeaf
bria.alyse <bria.alyse@fullsail.edu>
2014-02-21 23:53:40 -0500

    I entered the wacky conditional. If the person can eat a triple cheeseburgee, they must have a stomach that can contain 3 gallons, unless they haven't eaten in 4 hours.

---
+++ Conditionals_Wacky/js/script.js
@@ -0,0 +1,16 @@
+// Wacky Conditional
+
+var stomachSize = 3
+var minStomach = 2 
+var lastMealInHours = 4
+
+// In order to eat a triple cheeseburger, you need a min stomach size of 2 gallons.
+
+if(stomachSize > minStomach){
+	//if stomach size is larger than 3, you may eat a complete triple cheeseburger.
+	console.log("You have eaten an entire triple cheeseburger!")
+}else{(stomachSize < minStomach && lastMealInHours = 4)
+	console.log("you have shocked us and finished the entire burger!")
+
+}
+(stomachSize > minStomach) ? console.log("You have eaten an entire triple cheeseburger!") : console.log("You look green, don't try to finish the burger.")
