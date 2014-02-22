99a92ec6ff2fa1699f6c5dcdef4f3f439a50c0aa
bria.alyse <bria.alyse@fullsail.edu>
2014-02-21 23:30:48 -0500

    First commit for Industry Conditional. Added variables and if conditional.

---
+++ Conditionals_Industry/js/script.js
@@ -0,0 +1,12 @@
+// Industry Conditionals
+
+var adobeKnowledge = 75
+var minKnowledge = 50 
+var guideToAdobe = degree
+
+// in order to be a good web designer you need at least 50 in knowledge to understand what you are doing
+if(adobeKnowledge < 50){ 
+	console.log ("You will need more knowledge to be a good web designer")
+}else{
+
+}
