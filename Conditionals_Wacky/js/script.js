a8f28ebc7278f97f0034511b5df5c745d839859a
bria.alyse <bria.alyse@fullsail.edu>
2014-03-26 23:02:58 -0400

    Committing Wacky Conditional. All saved at different points. edited different small issues. Included logical, rational and ternary conditions. Variables included. Committing at once.

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,16 +1,14 @@
-// Wacky Conditional
+//Conditional Wacky
 
-var stomachSize = 3
-var minStomach = 2 
-var lastMealInHours = 4
+var laughsInADay = 11;
+var minLaughs = 10;
+var energyDrinks = 1;
 
-// In order to eat a triple cheeseburger, you need a min stomach size of 2 gallons.
-
-if(stomachSize > minStomach){
-	//if stomach size is larger than 3, you may eat a complete triple cheeseburger.
-	console.log("You have eaten an entire triple cheeseburger!")
-}else{(stomachSize < minStomach && lastMealInHours = 4)
-	console.log("you have shocked us and finished the entire burger!")
-
+if(laughsInADay < minLaughs){
+	//if laughs in a day is less than min laughs, print to console "why are you so serious??"
+	console.log("why are you so serious??")
+}else{
+	console.log("you are so silly today!")
 }
-(stomachSize > minStomach) ? console.log("You have eaten an entire triple cheeseburger!") : console.log("You look green, don't try to finish the burger.")
+
+(laughsInADay > minLaughs && energyDrinks) ? console.log("you are super hyper!") : console.log("are you tired??")
