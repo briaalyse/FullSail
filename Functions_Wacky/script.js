ed8cd195b20381524637c54405f9199c11819b00
bria.alyse <bria.alyse@fullsail.edu>
2014-03-28 23:54:16 -0400

    Edited small mistakes. Added console message.

--- Functions_Wacky/script.js
+++ Functions_Wacky/script.js
@@ -3,7 +3,8 @@
 //March 27,2014
 
 function numOfSocks(){
-	var numOfPacks = 20
-	var pairInPacks = 10
-	var numOfSocks = numOfSocks * pairInPacks
+	var numOfPacks = 20;
+	var pairInPacks = 10;
+	var numOfSocks = numOfSocks * pairInPacks;
+	console.log(numOfSocks);
 }
