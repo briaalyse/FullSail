dc61640a99deae0b579a122480fd1af4cfb28454
bria.alyse <bria.alyse@fullsail.edu>
2014-03-28 23:19:46 -0400

    Corrected some errors and added new console message.

--- Functions_Industry/script.js
+++ Functions_Industry/script.js
@@ -6,7 +6,7 @@ function monthsTilGraduation(){
 	//code the function runs
 	var numOfCourses = 26;
 	var lengthOfCourse = 1;
-	var monthsTilGraduation = numOfCourses * monthsTilGraduation
-	console.log(monthsTilGraduation)
+	var monthsTilGraduation = numOfCourses * lengthOfCourse
+	console.log("You will graduate in " + monthsTilGraduation + .")
 }
 
