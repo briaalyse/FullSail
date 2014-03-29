6e9710a12d228acc98190799f85222930a9702c5
bria.alyse <bria.alyse@fullsail.edu>
2014-03-28 23:10:42 -0400

    corrected some errors to improve calculations

--- Functions_Industry/script.js
+++ Functions_Industry/script.js
@@ -2,9 +2,11 @@
 //Bria Shaw
 //March 27,2014
 
-function monthsTilGraduation(numOfCourses * lengthOfCourse){
+function monthsTilGraduation(){
 	//code the function runs
-	var numOfCourses = 26
-	var lengthOfCourse = 1
+	var numOfCourses = 26;
+	var lengthOfCourse = 1;
+	var monthsTilGraduation = numOfCourses * monthsTilGraduation
 	console.log(monthsTilGraduation)
 }
+
