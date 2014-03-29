9cdc07773298ddb8328078d00f2f8bc1406a09e7
bria.alyse <bria.alyse@fullsail.edu>
2014-03-28 23:23:55 -0400

    Another modification to variable

--- Functions_Industry/script.js
+++ Functions_Industry/script.js
@@ -6,7 +6,7 @@ function monthsTilGraduation(){
 	//code the function runs
 	var numOfCourses = 26;
 	var accLengthOfCourse = 1;
-	var monthsTilGraduation = numOfCourses * lengthOfCourse
+	var monthsTilGraduation = numOfCourses * accLengthOfCourse
 	console.log("You will graduate in " + monthsTilGraduation + in the accelerated program.")
 }else if{
 
