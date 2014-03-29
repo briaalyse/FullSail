ca3f28102606ce2a2810cff3259c80917ce30d47
bria.alyse <bria.alyse@fullsail.edu>
2014-03-28 23:44:45 -0400

    Modified function into an anonymous function.

--- Functions_Personal/script.js
+++ Functions_Personal/script.js
@@ -2,11 +2,11 @@
 //Bria Shaw
 //March 21, 2014
 
-function weeklyPay(){
-	//code the function runs
-	var weeklyHours = 36;
-	var payPerHour = 10;
-	var overtimeHours = 4
-	var overtimePay = 15;
+function weeklyPay = function(weeklyHours * payPerHour + overtimeHours * overtimePay; 
+	)
 	var weeklyPay = weeklyHours * payPerHour + overtimeHours * overtimePay;
+	return weeklyPay;
 }
+
+var a = weeklyPay(40,10,4,15)
+console.log(a)
