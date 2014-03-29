1e2f0b0da18f8eaf205b4f56768e7ef30198aeda
bria.alyse <bria.alyse@fullsail.edu>
2014-03-28 23:39:03 -0400

    committing Personal Function. Added variables and functions.

---
+++ Functions_Personal/script.js
@@ -0,0 +1,12 @@
+//Personal Function
+//Bria Shaw
+//March 21, 2014
+
+function weeklyPay(){
+	//code the function runs
+	var weeklyHours = 36;
+	var payPerHour = 10;
+	var overtimeHours = 4
+	var overtimePay = 15;
+	var weeklyPay = weeklyHours * payPerHour + overtimeHours * overtimePay;
+}
