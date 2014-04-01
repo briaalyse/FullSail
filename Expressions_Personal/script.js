f24e655f10ec337201fec9d2272783e5164be154
bria.alyse <bria.alyse@fullsail.edu>
2014-03-31 21:18:09 -0400

    added the total sale total. Concatenated the result to make a full sentence for the total.

--- Expressions_Personal/script.js
+++ Expressions_Personal/script.js
@@ -8,3 +8,4 @@ var clothingPrice = [12.50, 29.99, 15.89];
 var percentOff = .15
 var salesTax = .06
 var clothingTotal = clothingPrice[0] + clothingPrice[1] + clothingPrice[2];
+var saleTotal = "Your total is " + (clothing total - (clothingTotal * percentOff) + (clothingTotal * salesTax)) +"."
