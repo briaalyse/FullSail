566416cd392ae07a211decb625bc83a4dc56924f
bria.alyse <bria.alyse@fullsail.edu>
2014-03-31 20:26:33 -0400

    added the comment on what the first variable represents.

--- Expressions_Industry/script.js
+++ Expressions_Industry/script.js
@@ -4,7 +4,7 @@
 
 //The balance in the account is the result of the deposits minus the withdrawals and fees
 
-var depositOfFunds = 3459;
-var withdrawalOfFunds = 2903;
-var accruedFees = 206;
+var depositOfFunds = 3459; //this is the sum of all of the checks and cash deposits made to the account. 
+var withdrawalOfFunds = 2903; 
+var accruedFees = 206; 
 var accountBalance = depositOfFunds - withdrawalOfFunds - accruedFees;
