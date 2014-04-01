ef71905c3dcc29cb79b4c030f05904e48c377623
bria.alyse <bria.alyse@fullsail.edu>
2014-04-01 19:44:53 -0400

    edited. added prompt.

--- Functions_Personal/script.js
+++ Functions_Personal/script.js
@@ -2,11 +2,12 @@
 //Bria Shaw
 //March 21, 2014
 
+var weeklyPay = prompt("enter weekly hours, pay per hour, overtime hours, overtime pay", [40, 10, 4, 15]);
 function weeklyPay = function(weeklyHours * payPerHour + overtimeHours * overtimePay; 
 	)
 	var weeklyPay = weeklyHours * payPerHour + overtimeHours * overtimePay;
 	return weeklyPay;
 }
 
-var a = weeklyPay(40,10,4,15)
-console.log(a)
+var a = weeklyPay(40,10,4,15);
+console.log(a);
