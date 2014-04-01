cffb1d8029683d2933e16dd55e59b9360ba4e6ea
bria.alyse <bria.alyse@fullsail.edu>
2014-04-01 17:51:57 -0400

    Corrected errors in my Wacky Expression.

--- Expressions_Wacky/script.js
+++ Expressions_Wacky/script.js
@@ -4,31 +4,32 @@
 
 //by choosing the month you were born and your favorite color, you will create a wacky name for yourself.
 
-var January = Slimy
-var February = Willy
-var March = Samurai
-var April = Cowboy
-var May = Silly
-var June = Linky
-var July = Skinny
-var August = Husky
-var September = Hunky
-var October = Majestic
-var November = Wet
-var December = Farmer
+var January = "Slimy";
+var February = "Willy";
+var March = "Samurai";
+var April = "Cowboy";
+var May = "Silly";
+var June = "Linky";
+var July = "Skinny";
+var August = "Husky";
+var September = "Hunky";
+var October = "Majestic";
+var November = "Wet";
+var December = "Farmer";
 
-var blue = Sunflower
-var red = Potato Head
-var green = Tall
-var orange = Tubby
-var purple = Toaster
-var yellow = Psycho
-var pink = Buttercup
-var black = Peanut
-var white = Rusty
-var gray = Buttons
-var silver = Four Eyes
-var gold = Peg Leg
-var brown = Baldy
+var blue = "Sunflower";
+var red = "Potato Head";
+var green = "Tall";
+var orange = "Tubby";
+var purple = "Toaster";
+var yellow = "Psycho";
+var pink = "Buttercup";
+var black = "Peanut";
+var white = "Rusty";
+var gray = "Buttons";
+var silver = "Four Eyes";
+var gold = "Peg Leg";
+var brown = "Baldy";
 
-var wackyName = "Hi, my name is " + String(December) + "-" + String(black) + "!" 
+var wackyName = "Hi, my name is " + String(December) + "-" + String(black) + "!"; 
+
