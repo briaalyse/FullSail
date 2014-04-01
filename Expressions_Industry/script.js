8bf5ccecf741885d21016cc0a0017a900a7f4cce
bria.alyse <bria.alyse@fullsail.edu>
2014-04-01 17:46:28 -0400

    corrected issues with my coding.

--- Expressions_Industry/script.js
+++ Expressions_Industry/script.js
@@ -9,6 +9,6 @@ var withdrawalOfFunds = 2903; //this is the sum of all of the cash and checks di
 var accruedFees = 206; //this the sum of the maintenance, membership and overdraft fees taken from the account.
 var accountBalance = "Your account balance is" + depositOfFunds - withdrawalOfFunds - accruedFees + ".";
 // the account balance can be found when you take the amount from the deposits, subtract the amount from 
-the withdrawals and subtract the amount from the fees. 
+//the withdrawals and subtract the amount from the fees. 
 console.log(accountBalance);
 
