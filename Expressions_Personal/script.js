ff0479ca2ec04454808f324485eab0881584f8be
bria.alyse <bria.alyse@fullsail.edu>
2014-03-31 21:01:11 -0400

    Added my name, and the comment on what my expression will be on. It will be based off what I have to spend when I do what I do best. Shop.

---
+++ Expressions_Personal/script.js
@@ -0,0 +1,7 @@
+//Personal Expression
+//Bria Shaw
+//March 31, 2014
+
+//In Connecticut,the sum of all the clothing I picked out, minus the percentage of the coupon, plus 6% tax is what I will have to spend on my clothing.
+
+var  
