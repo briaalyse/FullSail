ed6b0a14d7df2685d89052c41b03570c4c840c8c
bria.alyse <bria.alyse@fullsail.edu>
2014-03-31 21:28:07 -0400

    Created all of the month variables.

--- Expressions_Wacky/script.js
+++ Expressions_Wacky/script.js
@@ -3,3 +3,16 @@
 //March 31, 2014
 
 //by choosing the month you were born and your favorite color, you will create a wacky name for yourself.
+
+var January = Slimy
+var February = Willy
+var March = Samurai
+var April = Cowboy
+var May = Silly
+var June = Linky
+var July = Skinny
+var August = Husky
+var September = Hunky
+var October = Majestic
+var November = Wet
+var December = Farmer
