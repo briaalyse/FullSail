79cfb37b97e4a0fb57bdda2efb641734c3b0c9f8
bria.alyse <bria.alyse@fullsail.edu>
2014-04-01 17:47:43 -0400

    Corrected errors in my expression. added the console.log.

--- Expressions_Personal/script.js
+++ Expressions_Personal/script.js
@@ -5,7 +5,8 @@
 //In Connecticut,the sum of all the clothing I picked out, minus the percentage of the coupon, plus 6% tax is what I will have to spend on my clothing.
 
 var clothingPrice = [12.50, 29.99, 15.89];
-var percentOff = .15
-var salesTax = .06
+var percentOff = .15;
+var salesTax = .06;
 var clothingTotal = clothingPrice[0] + clothingPrice[1] + clothingPrice[2];
-var saleTotal = "Your total is " + (clothing total - (clothingTotal * percentOff) + (clothingTotal * salesTax)) +"."
+var saleTotal = "Your total is " + (clothing total - (clothingTotal * percentOff) + (clothingTotal * salesTax)) +".";
+console.log(saleTotal)
