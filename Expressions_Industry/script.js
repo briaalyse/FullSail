a891163f02d3859501a84003c6991a9a74fb283a
bria.alyse <bria.alyse@fullsail.edu>
2014-03-31 20:38:10 -0400

    added a comment to explain what the calculation was going to do. The result will be the account balance.

--- Expressions_Industry/script.js
+++ Expressions_Industry/script.js
@@ -7,4 +7,4 @@
 var depositOfFunds = 3459; //this is the sum of all of the checks and cash deposits made to the account. 
 var withdrawalOfFunds = 2903; //this is the sum of all of the cash and checks disbursed from the account. 
 var accruedFees = 206; //this the sum of the maintenance, membership and overdraft fees taken from the account.
-var accountBalance = depositOfFunds - withdrawalOfFunds - accruedFees;
+var accountBalance = depositOfFunds - withdrawalOfFunds - accruedFees; // the account balance can be found when you take the amount from the deposits, subtract the amount from the withdrawals and subtract the amount from the fees. 
