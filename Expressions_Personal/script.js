7db3f3eeb57e5d362e66a5a85fdfed40ceccf5aa
bria.alyse <bria.alyse@fullsail.edu>
2014-03-31 21:04:04 -0400

    added second variable.

--- Expressions_Personal/script.js
+++ Expressions_Personal/script.js
@@ -5,3 +5,5 @@
 //In Connecticut,the sum of all the clothing I picked out, minus the percentage of the coupon, plus 6% tax is what I will have to spend on my clothing.
 
 var clothingPrice = [12.50, 29.99, 15.89];
+var percentOff= .15
+
